647112c0bd4594b9bc7a3f3e11688ed5ed293596
sanju464 <sanjuthomas459@gmail.com>
2026-02-25 12:42:01 +0000

    Time: 0 ms (100.00%) | Memory: 12.6 MB (17.64%) - LeetX Squads

---
+++ Easy/0027-remove-element/remove-element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeElement(self, nums, val):
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] == val:
+                nums.pop(i)
+
+        return len(nums)
