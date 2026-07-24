3cc1f8730f3993b71b09a078deea7e7d03a4aeec
sanju464 <sanjuthomas459@gmail.com>
2026-07-24 14:32:24 +0000

    Time: 0 ms (100.00%) | Memory: 15 MB (65.02%) - LeetX Squads

---
+++ Easy/1929-concatenation-of-array/concatenation-of-array.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    *returnSize = 2 * numsSize;
+
+    int* ans = (int*)malloc((*returnSize) * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++) {
+        ans[i] = nums[i];
+        ans[i + numsSize] = nums[i];
+    }
+
+    return ans;
+}
